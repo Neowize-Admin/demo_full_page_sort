0fa6054fbb4190699c5c8385b26e6709d2ac14b3
Ronen Ness <ronenness@gmail.com>
2017-08-29 15:15:19 +0300

    ..

---
+++ NeoWize_files/analytics.js
@@ -0,0 +1,79 @@
+// GOOGLE ANALYTICS
+try
+{
+    (function() {
+
+        // get google analytics
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        // init and send visit
+        ga('create', 'UA-105570970-1', 'auto');
+        ga('send', 'pageview');
+
+        // attach ga to global scope
+        window.ga = ga;
+
+        // helper function to send google analytics event
+        window.sendGaEvent = function(action) {
+            window.ga('send', 'event', "user_event", action, location.host);
+        }
+
+    })();
+}
+catch (e)
+{
+    console.warn("Error with google analytics!", e);
+    window.ga = function() {};
+}
+
+// HOTJAR
+(function() {
+
+    // guarantree.neowize.com
+    if (location.host == "guarantee.neowize.com") {
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:607867,hjsv:5};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+    }
+    // newusers.neowize.com
+    else if (location.host == "guarantee.neowize.com") {
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:607871,hjsv:5};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+    }
+    // easyintegration.neowize.com
+    else if (location.host == "guarantee.neowize.com") {
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:607875,hjsv:5};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+    }
+    // categorysort.neowize.com
+    else if (location.host == "guarantee.neowize.com") {
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:607877,hjsv:5};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+    }
+})();
